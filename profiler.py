3b2f5cd68fd751ea4e59e753953a4a16f5de504a
Oana Buturas <oana.buturas@gmail.com>
2018-05-25 08:07:25 +0300

    added summery for the program

---
+++ profiler.py
@@ -0,0 +1,8 @@
+"""
+Defines a class for profiling sort algorithms.
+A Profiler object tracks the list, the number of
+comparisons and exchanges and the running time. The 
+Profiler can also print a trace and can create a list
+of unique or duplicate numbers. 
+"""
+
