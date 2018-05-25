12cdaa70f1e946ad6ddd567c00aa2d75292446ad
Oana Buturas <oana.buturas@gmail.com>
2018-05-25 08:36:21 +0300

    fields

--- profiler.py
+++ profiler.py
@@ -11,7 +11,7 @@ import random
 
 class Profiler():
     def test(self, function, lyst=None, size=10, unique=True, comp=True, exch=True, trace=False):
-        
+
         """
         function: the algorithm being profiled
         lyst: allow calling a list
@@ -22,6 +22,24 @@ class Profiler():
         trace: if true, print the list after each exchange
         """
 
+        self.comp = comp
+        self.exch = exch
+        self.trace = trace
+        if lyst != None:
+            self.lyst = lyst
+        elif unique:
+            self.lyst = range(1, size + 1)
+            random.shuffle(self.lyst)
+        else: 
+            self.lyst = []
+            for count in range(size):
+                self.lyst.append(random.randint(1, size))
 
+        self.exchCount = 0
+        self.cmpCount = 0
+        self.startClock()
+        function(self.lyst, self)
+        self.stopClock()
+        print(self)
 
 
