250bf7830f48144c4f2cece926177878573e6790
Oana Buturas <oana.buturas@gmail.com>
2018-05-25 08:20:08 +0300

    added summery for the class

--- profiler.py
+++ profiler.py
@@ -6,3 +6,22 @@ Profiler can also print a trace and can create a list
 of unique or duplicate numbers. 
 """
 
+import time
+import random
+
+class Profiler():
+    def test(self, function, lyst=None, size=10, unique=True, comp=True, exch=True, trace=False):
+        
+        """
+        function: the algorithm being profiled
+        lyst: allow calling a list
+        size: the size of the list
+        unique: if true, the list contains unique integers
+        comp: if true, count comparisons
+        exch: if true, count exchanges
+        trace: if true, print the list after each exchange
+        """
+
+
+
+
