6f2c3a8de59c7598e9a3009c848d081b86478af2
Oana Buturas <oana.buturas@gmail.com>
2018-06-19 14:33:25 +0300

    added methods

--- linkedList.py
+++ linkedList.py
@@ -3,16 +3,22 @@ class Node:
         self.data = data      
         self.next_node = next        
 
-class LinkedList:
-    def __init__(self, root = None):
-        self.root = root
+class TwoWayNode(Node):
+    def __init__(self, data, previous=None, next = None):
+        Node.__init__(self, data, next)
+        self.previous = previous
         self.size = 0
 
     def length(self):
         return self.size
 
-    def add(self, data):
-        new_node = Node(data, self.root)
-        self.root = new_node
-        self.size += 1
+    def insert(self, item, index):
+        pass
+        #if index >= self.size:
+    
+    def pop(self, index):
+        pass
+
+    def makeTwoWay(self):
+        pass
     
